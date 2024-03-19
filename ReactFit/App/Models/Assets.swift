0bbb933b13e14bea0473735fe030006f1407b0b9
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-03-19 07:25:36 -0500

    files removed from .gitignore

--- ReactFit/App/Models/Assets.swift
+++
@@ -1,5 +0,0 @@
-import SwiftUI
-
-enum Assets {
-    static let gym1 = Image("gym-1")
-}
--- ReactFit/App/Models/Fonts.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-enum Fonts {
-    static let sourceCodePro = "SourceCodePro-SemiBold"
-}
